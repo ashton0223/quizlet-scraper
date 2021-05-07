920a26096576316ebc5dd22fab7c6cde1b35005e
Ashton S <aasouth223@gmail.com>
2021-05-07 11:32:41 -0500

    refractoring

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import quizlet_terms as q
 import spreadsheet
 import time
+from os import path
 
 input_text = """
 What filetype do you want to export to?
@@ -10,6 +11,22 @@ What filetype do you want to export to?
 4 for .apkg (Anki package)
 5 to quit
 """
+
+#Asks for filename and returns it. Also checks if it exists
+def get_filename(filetype):
+    filename = input('Filename: ')
+    check_file(filename + filetype)
+    return filename + filetype
+
+def get_deck_name():
+    return input('Deck name: ')
+
+#Checks if file exists, if it does not, throw exception
+def check_file(filename):
+    if(path.exists(filename)):
+        print('File already exists')
+        raise Exception("File already exists")
+
 
 def main():
     URL = input('Please enter a quizlet URL:\n--> ')
@@ -32,16 +49,21 @@ def main():
             if (filetype < 1 or filetype > 5):
                 raise Exception('Invalid selection')
             elif (filetype == 1):
-                spreadsheet.write_to_csv(term_list)
+                name = get_filename('.csv')
+                spreadsheet.write_csv(term_list, name)
                 break
             elif (filetype == 2):
-                spreadsheet.write_to_txt_spreadsheet(term_list)
+                name = get_filename('.txt')
+                spreadsheet.write_txt(term_list, name)
                 break
             elif (filetype == 3):
-                spreadsheet.write_to_xls_spreadsheet(term_list)
+                name = get_filename('.xls')
+                spreadsheet.write_xls(term_list, name)
                 break
             elif (filetype == 4):
-                spreadsheet.write_to_anki(term_list)
+                deck_name = get_deck_name()
+                name = get_filename('.apkg')
+                spreadsheet.write_anki(term_list, deck_name, name)
                 break
             elif (filetype == 5):
                 break
--- spreadsheet.py
+++ spreadsheet.py
@@ -1,5 +1,3 @@
-import os.path
-from os import path
 import xlwt
 from xlwt import Workbook
 from classes import term
@@ -7,9 +5,7 @@ import csv
 import genanki
 
 #Writes a list of terms to a .xls spreadsheet
-def write_to_xls_spreadsheet(term_list):
-    filename = get_filename('.xls')
-
+def write_xls(term_list, name):
     wb = Workbook()
 
     sheet = wb.add_sheet('Sheet')
@@ -21,26 +17,24 @@ def write_to_xls_spreadsheet(term_list):
         sheet.write(x, 1, i.definition)
         x += 1
 
-    wb.save(filename)
+    wb.save(name)
 
 #Writes a list of terms to a .txt tab delimited spreadsheet
-def write_to_txt_spreadsheet(term_list):
-    filename = get_filename('.txt')
-    f = open(filename, 'w+')
+def write_txt(term_list, name):
+    f = open(name, 'w+')
 
     #Write terms and definitions
     for i in term_list:
         f.write(i.word + '\t' + i.definition + '\n')
     f.close()
 
-def write_to_csv(term_list):
-    filename = get_filename('.csv')
-    with open(filename, 'w+', newline='') as file:
+def write_csv(term_list, name):
+    with open(name, 'w+', newline='') as file:
         writer = csv.writer(file)
         for i in term_list:
             writer.writerow([i.word, i.definition])
 
-def write_to_anki(term_list):
+def write_anki(term_list, deck_name, name):
     model = genanki.Model(
         2123350969,
         'Model',
@@ -58,7 +52,7 @@ def write_to_anki(term_list):
     )
     deck = genanki.Deck(
         1164622816,
-        get_deck_name()
+        deck_name
     )
     for i in term_list:
         note = genanki.Note(
@@ -67,20 +61,4 @@ def write_to_anki(term_list):
         )
         deck.add_note(note)
     
-    genanki.Package(deck).write_to_file(get_filename('.apkg'))
-
-    
-#Asks for filename and returns it. Also checks if it exists
-def get_filename(filetype):
-    filename = input('Filename: ')
-    check_file(filename + filetype)
-    return filename + filetype
-
-def get_deck_name():
-    return input('Deck name: ')
-
-#Checks if file exists, if it does not, throw exception
-def check_file(filename):
-    if(path.exists(filename)):
-        print('File already exists')
-        raise Exception("File already exists")
+    genanki.Package(deck).write_to_file(name)
